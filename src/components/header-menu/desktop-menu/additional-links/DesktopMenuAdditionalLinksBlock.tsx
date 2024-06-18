8e8e3ca872375347e5422e41366475fe5a541e7d
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2024-06-18 13:39:04 +0300

    (chore) lint fix

--- src/components/header-menu/desktop-menu/additional-links/DesktopMenuAdditionalLinksBlock.tsx
+++ src/components/header-menu/desktop-menu/additional-links/DesktopMenuAdditionalLinksBlock.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { CmsLink } from "@/components/cms/link/CmsLink";
 import { CmsLinkBlockFragment } from "@/types";
-import { isTypename } from "@/types/graphql-helpers";
+import { filterByTypename } from "@/types/graphql-helpers";
 
 interface Props {
   block: CmsLinkBlockFragment;
@@ -11,8 +11,8 @@ interface Props {
 export const DesktopMenuAdditionalLinksBlock: React.FC<Props> = ({ block }) => {
   return (
     <ul className="list-none flex gap-6 items-center">
-      {block.links.map((link, idx) => {
-        if (isTypename(link, ["Link"])) {
+      {block.links &&
+        filterByTypename(block.links, "Link")?.map((link, idx) => {
           return (
             <li key={idx}>
               <CmsLink
@@ -23,10 +23,7 @@ export const DesktopMenuAdditionalLinksBlock: React.FC<Props> = ({ block }) => {
               />
             </li>
           );
-        }
-
-        return null;
-      })}
+        })}
     </ul>
   );
 };
