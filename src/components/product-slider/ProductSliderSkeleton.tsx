28512983452534958a1da38c768d3a0a216e5257
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2024-06-19 10:19:51 +0300

    Added product slider loader skeleton

--- src/components/product-slider/ProductSliderSkeleton.tsx
+++ src/components/product-slider/ProductSliderSkeleton.tsx
@@ -4,17 +4,17 @@ export const ProductSliderSkeleton = () => {
   return (
     <div className="my-28 product-slider relative">
       <h2 className="text-2xl font-medium font-feature mb-4 lg:mb-8">
-        <span className="animate-pulse bg-warm-grey h-8 w-1/3 block" />
+        <span className="animate-pulse bg-warm-grey h-8 w-1/2 lg:w-1/3 block" />
       </h2>
       <div className="hidden lg:block">
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-8">
+        <div className="grid grid-cols-4 gap-8">
           {[1, 2, 3, 4].map((_, idx) => (
             <ProductCardSkeleton key={idx} />
           ))}
         </div>
       </div>
       <div className="lg:hidden">
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-8">
+        <div className="grid grid-cols-2 gap-4">
           {[1, 2].map((_, idx) => (
             <ProductCardSkeleton key={idx} />
           ))}
--- src/components/product/ProductCardSkeleton.tsx
+++ src/components/product/ProductCardSkeleton.tsx
@@ -1,7 +1,7 @@
 export const ProductCardSkeleton = () => {
   return (
-    <div className="w-[320px]">
-      <div className="animate-pulse bg-warm-grey rounded-2xl h-[420px] w-full block" />
+    <div className="w-full">
+      <div className="animate-pulse bg-warm-grey rounded-2xl h-[240px] lg:h-[420px] w-full block" />
       <div className="animate-pulse bg-warm-grey h-4 w-3/4 mt-2 block" />
       <div className="animate-pulse bg-warm-grey h-4 w-1/2 mt-2 block" />
       <div className="animate-pulse bg-warm-grey h-6 mt-4 w-1/4 block" />
