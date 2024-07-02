95b7c65cc0173089bf686915cfa0f2ecf91bb0ae
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2024-07-02 08:42:56 +0300

    Cart init

--- src/modules/cart/CartItem.tsx
+++ src/modules/cart/CartItem.tsx
@@ -29,11 +29,11 @@ export const CartItem: React.FC<Props> = ({ item }) => {
     >
       <Link
         href={item.product.canonical_url ?? ""}
-        className="relative p-6 w-[160px] h-[160px] bg-warm-grey rounded-3xl !flex justify-center items-center"
+        className="relative p-6 w-[70px] lg:w-[160px] h-[70px] lg:h-[160px] bg-warm-grey rounded-xl lg:rounded-3xl !flex justify-center items-center"
       >
         {item?.product.image?.url && (
           <Image
-            className="object-contain h-[100px] w-[120px]"
+            className="object-contain h-[50px] lg:h-[120px] w-[120px]"
             width={200}
             height={200}
             src={item?.product.image?.url}
@@ -46,7 +46,7 @@ export const CartItem: React.FC<Props> = ({ item }) => {
           <div className="flex flex-col gap-2">
             <Link
               href={item.product.canonical_url ?? ""}
-              className="text-lg font-semibold"
+              className="text-sm lg:text-lg font-semibold"
             >
               {item?.product.name}
             </Link>
@@ -57,17 +57,17 @@ export const CartItem: React.FC<Props> = ({ item }) => {
               }}
             />
           </div>
-          <div className="w-full flex items-center gap-4">
+          <div className="w-full lg:text-base flex items-center gap-4 mt-4">
             <QuantityInput value={String(item?.quantity ?? 1)} />
             <button
               onClick={handleRemoveProduct}
-              className="underline text-black"
+              className="underline text-black text-xs lg:text-base"
             >
               Fjern produkt
             </button>
           </div>
         </div>
-        <div className="flex flex-col items-end min-w-[120px] text-sm font-semibold whitespace-nowrap">
+        <div className="flex flex-col items-end min-w-[80px] lg:min-w-[120px] text-xs lg:text-sm font-semibold whitespace-nowrap">
           <FormatNumber
             value={item?.prices?.row_total_including_tax.value}
             format="currency"
