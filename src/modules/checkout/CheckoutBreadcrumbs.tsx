5b255d33e20fa7da7c20ebadc31263a45be38bfe
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2024-07-23 12:52:39 +0300

    (chore): Cart preferred method

--- src/modules/checkout/CheckoutBreadcrumbs.tsx
+++ src/modules/checkout/CheckoutBreadcrumbs.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 
+import { cookies } from "next/headers";
+
 import { Breadcrumbs } from "@/components/breadcrumbs";
 
 export const CheckoutBreadcrumbs = () => {
+  const cookiesStore = cookies();
+  const preferredMethod = cookiesStore.get("preferredMethod");
+
   return (
     <Breadcrumbs
       data={[
         {
           label: "Handlekurv",
-          url: "/cart",
+          url: preferredMethod?.value
+            ? `/cart?method=${preferredMethod?.value}`
+            : "/cart",
         },
       ]}
     />
