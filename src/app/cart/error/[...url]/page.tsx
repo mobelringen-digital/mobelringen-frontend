6ad1d77b107f07139f8a9170cc88cd1ba48f5bf0
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2024-08-07 09:05:48 +0300

    (chore): Revalidate cache on success/error page

--- src/app/cart/error/[...url]/page.tsx
+++ src/app/cart/error/[...url]/page.tsx
@@ -1,5 +1,9 @@
+import { revalidateTag } from "next/cache";
+
 import { CartErrorPage } from "@/modules/cart/error/CartErrorPage";
 
 export default function CartError() {
+  revalidateTag("cart");
+
   return <CartErrorPage />;
 }
--- src/app/cart/success/[...url]/page.tsx
+++ src/app/cart/success/[...url]/page.tsx
@@ -1,5 +1,9 @@
+import { revalidateTag } from "next/cache";
+
 import { CartSuccessPage } from "@/modules/cart/success/CartSuccessPage";
 
 export default function CartSuccess() {
+  revalidateTag("cart");
+
   return <CartSuccessPage />;
 }
