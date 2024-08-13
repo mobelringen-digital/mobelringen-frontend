0e7a47a55a2028d00e42414de9e9f9ba2b81398b
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2024-08-13 12:15:57 +0300

    (chore): Missing text alignment

--- src/components/cms/__components/text-block/index.tsx
+++ src/components/cms/__components/text-block/index.tsx
@@ -3,39 +3,46 @@ import React from "react";
 import cx from "classnames";
 
 import { CmsLink } from "@/components/cms/__components/link/CmsLink";
-import { CmsTextBlockFragment } from "@/types";
+import { CmsTextBlockFragment, Position } from "@/types";
 
 interface Props {
   data: CmsTextBlockFragment;
 }
 
-const TEXT_ALIGN: Record<string, string> = {
-  left: "text-left",
-  center: "text-center",
-  right: "text-right",
+const TEXT_ALIGN: Record<Position, string> = {
+  LEFT: "text-left",
+  CENTER: "text-center",
+  RIGHT: "text-right",
 };
 
-const BUTTON_JUSTIFY: Record<string, string> = {
-  left: "justify-start",
-  center: "justify-center",
-  right: "justify-end",
+const BUTTON_JUSTIFY: Record<Position, string> = {
+  LEFT: "justify-start",
+  CENTER: "justify-center",
+  RIGHT: "justify-end",
 };
 
 export const TextBlock: React.FC<Props> = ({ data }) => {
   return (
     <div className="flex flex-col justify-center gap-8">
       {data.title ? (
-        <h2 className="text-4xl font-medium font-feature">{data.title}</h2>
+        <h2
+          className={cx(
+            "text-4xl font-medium font-feature",
+            TEXT_ALIGN[data.textAlign ?? "LEFT"],
+          )}
+        >
+          {data.title}
+        </h2>
       ) : null}
       {data.content?.html ? (
         <div
-          className={cx("prose text-xl", TEXT_ALIGN[data.textAlign ?? "left"])}
+          className={cx("prose text-xl", TEXT_ALIGN[data.textAlign ?? "LEFT"])}
           dangerouslySetInnerHTML={{ __html: data.content.html }}
         />
       ) : null}
       {data.links ? (
         <div
-          className={cx("flex gap-4", BUTTON_JUSTIFY[data.textAlign ?? "left"])}
+          className={cx("flex gap-4", BUTTON_JUSTIFY[data.textAlign ?? "LEFT"])}
         >
           {data.links.map((link, idx) => (
             <CmsLink
