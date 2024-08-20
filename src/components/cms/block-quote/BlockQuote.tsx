5f5f240dd4fbe0c5a3e1d880a1fb18c63d01d09e
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2024-08-20 10:20:38 +0300

    (chore): Quote with no image

--- src/components/cms/block-quote/BlockQuote.tsx
+++ src/components/cms/block-quote/BlockQuote.tsx
@@ -13,6 +13,29 @@ interface Props {
 }
 
 export const BlockQuote: React.FC<Props> = ({ data }) => {
+  const BlockQuoteComponent = ({ hasImage }: { hasImage: boolean }) => {
+    return (
+      <blockquote
+        className={cx(
+          {
+            "absolute lg:right-0 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-3/4 lg:mx-0 top-full -translate-y-1/4 mx-4":
+              hasImage,
+          },
+          "bg-powder lg:w-[800px] p-6 lg:p-12 rounded-[56px] rounded-bl-none font-feature flex font-normal justify-center items-center flex-col gap-4",
+        )}
+      >
+        <Quote />
+        <p
+          className="text-xl lg:text-3xl text-center mt-2"
+          dangerouslySetInnerHTML={{
+            __html: data.quote?.html ?? "",
+          }}
+        />
+        <p className="text-center">- {data.author}</p>
+      </blockquote>
+    );
+  };
+
   if (data.image?.url) {
     return (
       <CmsBlockWrapper>
@@ -24,26 +47,21 @@ export const BlockQuote: React.FC<Props> = ({ data }) => {
               src={data.image.url}
               alt={data.author ?? "quote"}
             />
-            <blockquote
-              className={cx(
-                "absolute lg:right-0 lg:top-1/2 lg:-translate-y-1/2 lg:w-[800px] lg:translate-x-3/4 lg:mx-0",
-                "top-full -translate-y-1/4 mx-4",
-                "bg-powder p-6 lg:p-12 rounded-[56px] rounded-bl-none font-feature flex font-normal justify-center items-center flex-col gap-4",
-              )}
-            >
-              <Quote />
-              <p
-                className="text-xl lg:text-3xl text-center mt-2"
-                dangerouslySetInnerHTML={{
-                  __html: data.quote?.html ?? "",
-                }}
-              />
-              <p className="text-center">- {data.author}</p>
-            </blockquote>
+            <BlockQuoteComponent hasImage={true} />
           </div>
           <div />
         </div>
       </CmsBlockWrapper>
     );
   }
+
+  return (
+    <CmsBlockWrapper>
+      <div className="grid grid-cols-1 text-center">
+        <div className="flex justify-center">
+          <BlockQuoteComponent hasImage={false} />
+        </div>
+      </div>
+    </CmsBlockWrapper>
+  );
 };
