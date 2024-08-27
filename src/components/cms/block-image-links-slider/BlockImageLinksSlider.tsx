03a41e122292e2a0bd5f187a1826ab1169fc085b
Vitalijus Zimaitis <vitalijus.zimaitis@frontit.dk>
2024-08-27 15:13:42 +0300

    (feature): Block image links slider

--- src/components/cms/block-image-links-slider/BlockImageLinksSlider.tsx
+++ src/components/cms/block-image-links-slider/BlockImageLinksSlider.tsx
@@ -21,16 +21,23 @@ const sliderConfig: Settings = {
   dots: false,
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 1280,
       settings: {
         slidesToShow: 4,
         slidesToScroll: 1,
       },
     },
     {
-      breakpoint: 768,
+      breakpoint: 1024,
       settings: {
         slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
         slidesToScroll: 1,
         arrows: false,
       },
