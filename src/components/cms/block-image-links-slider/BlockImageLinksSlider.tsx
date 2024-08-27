d7141f07da90d48229c2c84d4091d87d197a6677
Vitalijus Zimaitis <vitalijus.zimaitis@frontit.dk>
2024-08-27 15:15:02 +0300

    (chore): Mobile font size

--- src/components/cms/block-image-links-slider/BlockImageLinksSlider.tsx
+++ src/components/cms/block-image-links-slider/BlockImageLinksSlider.tsx
@@ -66,7 +66,7 @@ export const BlockImageLinksSlider: React.FC<Props> = ({ data }) => {
             {data && isTypename(imageData, ["ImageLink"]) ? (
               <Link href={imageData.url} className="relative">
                 <div className="rounded-2xl absolute left-0 right-0 top-0 bottom-0 bg-black bg-opacity-20 pointer-events-none flex justify-start items-end">
-                  <span className="font-medium text-4xl text-white font-feature p-4">
+                  <span className="font-medium text-2xl lg:text-4xl text-white font-feature p-4">
                     {imageData.label}
                   </span>
                 </div>
