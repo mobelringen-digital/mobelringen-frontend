38629f5a851cf8c22df9e1d2b70581601f4c6ee2
Vitalijus Zimaitis <vitalijus.zimaitis@frontit.dk>
2024-08-28 17:58:36 +0300

    (chore): Static page content

--- src/components/cms/static-page-content/StaticPageContent.tsx
+++ src/components/cms/static-page-content/StaticPageContent.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { CmsContentLoader } from "@/components/cms/cms-content-loader";
 import { getPage } from "@/components/cms/static-page-content/actions";
+import { MetaDescription, MetaTitle } from "@/components/meta";
 
 interface Props {
   url: string;
@@ -9,6 +10,8 @@ interface Props {
 
 export async function StaticPageContent({ url }: Props) {
   const data = await getPage(url);
+  const metaDescription = data.pages[0]?.metaDescription;
+  const title = data.pages[0]?.metaTitle;
 
   if (!data.pages[0]) {
     return null;
@@ -16,6 +19,11 @@ export async function StaticPageContent({ url }: Props) {
 
   return (
     <>
+      {title ? <MetaTitle title={title} /> : null}
+      {metaDescription ? (
+        <MetaDescription description={metaDescription} />
+      ) : null}
+
       {data.pages[0]?.content.map((content) => {
         return <CmsContentLoader key={content.__typename} data={content} />;
       })}
