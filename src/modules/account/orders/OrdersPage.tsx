be09c863f64161f9ff1ba3672f938fc6cfbb08de
Vitalijus Zimaitis <vitalijus.zimaitis@frontit.dk>
2024-09-19 10:31:58 +0300

    (feature): Order categories

--- src/modules/account/orders/OrdersPage.tsx
+++ src/modules/account/orders/OrdersPage.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import React from "react";
+
+import cx from "classnames";
 
 import { Debugger } from "@/components/Debugger";
 import { AccountPageLayout } from "@/modules/account/components/AccountPageLayout";
@@ -9,13 +13,54 @@ interface Props {
   orders?: Array<CustomerOrderFragment | null>;
 }
 
+const CATEGORIES = [
+  {
+    type: "Online",
+    title: "På nett",
+  },
+  {
+    type: "Cac",
+    title: "Klikk og hent",
+  },
+  {
+    type: "OIP",
+    title: "I butikk",
+  },
+];
+
 export const OrdersPage: React.FC<Props> = ({ orders }) => {
+  const [activeCategory, setActiveCategory] = React.useState("Online");
+
+  const getCount = (type: string) => {
+    return orders?.filter((order) => order?.delivery_type === type).length;
+  };
+
   return (
     <AccountPageLayout title="Mine bestillinger">
-      <div className="flex flex-col gap-4">
-        {orders?.map((order) => {
-          return <OrderItem data={order} key={order?.id} />;
+      <div className="flex gap-6">
+        {CATEGORIES.map((category) => {
+          return (
+            <button
+              onClick={() => setActiveCategory(category.type)}
+              key={category.type}
+              className={cx("font-feature text-2xl", {
+                underline: activeCategory === category.type,
+              })}
+            >
+              {category.title} ({getCount(category.type)})
+            </button>
+          );
         })}
+      </div>
+
+      <div className="flex flex-col gap-4">
+        {orders
+          ?.filter((o) =>
+            o?.delivery_type ? o?.delivery_type === activeCategory : o,
+          )
+          .map((order) => {
+            return <OrderItem data={order} key={order?.id} />;
+          })}
       </div>
       <Debugger data={orders} />
     </AccountPageLayout>
