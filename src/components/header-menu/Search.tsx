fef9cd076b4a9c3c1ef77df59034409e2b95c769
Vitalijus Zimaitis <vitalijus.zimaitis@frontit.dk>
2024-09-24 13:30:20 +0300

    (fix): Search query corrections

--- src/components/header-menu/Search.tsx
+++ src/components/header-menu/Search.tsx
@@ -1,12 +1,19 @@
+"use client";
+
 import React from "react";
+
+import { useSearchParams } from "next/navigation";
 
 import { SearchIcon } from "@/components/_ui/icons/SearchIcon";
 import { Input } from "@/components/_ui/input/Input";
 
 export const Search = () => {
+  const searchParams = useSearchParams();
+
   return (
     <form method="GET" action="/search">
       <Input
+        value={searchParams.get("q") || ""}
         startContent={
           <button type="submit">
             <SearchIcon />
