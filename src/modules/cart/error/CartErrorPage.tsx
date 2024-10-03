3680c4c8a5406e60e20b1419ff3f77d46e9938cc
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2024-10-03 17:28:56 +0300

    (chore): Try catch cart success page

--- src/modules/cart/error/CartErrorPage.tsx
+++ src/modules/cart/error/CartErrorPage.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+import Link from "next/link";
+
+import { Button } from "@/components/_ui/button/Button";
 import { ContainerLayout } from "@/components/layouts/ContainerLayout";
 import { PageTitle } from "@/components/typography/PageTitle";
 import { CartBreadcrumbs } from "@/modules/cart/CartBreadcrumbs";
@@ -9,6 +12,14 @@ export const CartErrorPage = () => {
     <ContainerLayout>
       <CartBreadcrumbs />
       <PageTitle>Noe gikk galt</PageTitle>
+      <p className="text-lg flex justify-center">
+        Det ser ut til at noe gikk galt. Vennligst prøv igjen senere.
+      </p>
+      <div className="flex my-12 justify-center">
+        <Button as={Link} href="/">
+          Til forsiden
+        </Button>
+      </div>
     </ContainerLayout>
   );
 };
