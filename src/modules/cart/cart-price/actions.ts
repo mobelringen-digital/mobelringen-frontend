5eade5b3c99aae8958bfbb9b6a50e9c2925e5392
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2024-10-07 16:04:44 +0300

    (feature): Revalidate cart cache after validation

--- src/modules/cart/cart-price/actions.ts
+++ src/modules/cart/cart-price/actions.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import {revalidateTag} from "next/cache";
+
 import { ValidateCartDocument } from "@/types";
 import { baseMagentoClient } from "@/utils/lib/graphql";
 
@@ -7,6 +9,8 @@ export async function validateCart(cartId: string) {
   const data = await baseMagentoClient("POST").request(ValidateCartDocument, {
     cart_id: cartId,
   });
+
+  revalidateTag("cart");
 
   return data.validateCart;
 }
