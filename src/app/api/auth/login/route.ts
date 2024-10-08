47e7a97b53dfa0fb7e81db2254e43ed2997a6531
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2024-10-08 08:51:04 +0300

    (chore): Login caching

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -9,6 +9,7 @@ import { baseMagentoClient } from "@/utils/lib/graphql";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
+export const fetchCache = "only-no-store";
 
 export async function POST(request: NextRequest) {
   const { email, password } = await request.json();
