8d0e8b17f999f5fc71512dc99e468f743a3ccb7c
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2024-10-10 17:41:50 +0300

    (chore): robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,9 +1,10 @@
 export default function robots() {
   return {
     rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: "/private/",
+      userAgent: "Baiduspider",
+      disallow: "/",
+      "User-agent": "*",
+      "Crawl-delay": 10,
     },
     sitemap: `${process.env.NEXT_PUBLIC_APP_URL}/sitemap.xml`,
   };
