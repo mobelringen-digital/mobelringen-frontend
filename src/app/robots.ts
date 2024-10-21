b051b0c2961bfda0d64fbdab68a2d5219510d9e0
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2024-10-21 16:41:12 +0300

    Products sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,5 +7,6 @@ export default function robots() {
       "Crawl-delay": 10,
     },
     sitemap: `${process.env.NEXT_PUBLIC_APP_URL}/sitemap.xml`,
+    sitemap_south: `${process.env.NEXT_PUBLIC_BACKEND_URL}/sitemap_south.xml`,
   };
 }
