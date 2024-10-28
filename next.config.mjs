ce20a97d332d5560588f10dab3f5ebf7045c9110
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2024-10-28 12:53:14 +0200

    MBEL-696 Try new cache headers for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,20 @@ const nextConfig = {
   experimental: {
     scrollRestoration: false,
   },
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=3600, stale-while-revalidate=3600",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     unoptimized: true,
     dangerouslyAllowSVG: true,
