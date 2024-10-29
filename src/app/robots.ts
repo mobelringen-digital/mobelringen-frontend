46e039dbe5df2baea7d0298c60d1c30f2a70b382
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2024-10-29 09:03:15 +0200

    MBEL-726 Robots hotfix

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,27 @@
 export default function robots() {
   return {
-    rules: {
-      userAgent: "Baiduspider",
-      disallow: "/",
-      "User-agent": "*",
-      "Crawl-delay": 10,
-    },
+    rules: [
+      {
+        userAgent: "*",
+        disallow: "/*?*",
+      },
+      {
+        userAgent: "*",
+        disallow: "/search",
+      },
+      {
+        userAgent: "*",
+        disallow: "/account",
+      },
+      {
+        userAgent: "*",
+        disallow: "/cart",
+      },
+      {
+        userAgent: "*",
+        disallow: "/auth",
+      },
+    ],
     sitemap: `${process.env.NEXT_PUBLIC_APP_URL}/sitemap.xml`,
     sitemap_south: `${process.env.NEXT_PUBLIC_BACKEND_URL}/sitemap_south.xml`,
   };
