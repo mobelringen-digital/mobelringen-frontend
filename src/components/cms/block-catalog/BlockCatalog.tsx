71aff4b3559795d223cdf3a5f3694b29462965b5
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2024-10-29 09:41:06 +0200

    MBEL-721 catalog tweaks

--- src/components/cms/block-catalog/BlockCatalog.tsx
+++ src/components/cms/block-catalog/BlockCatalog.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import cx from "classnames";
+
 import Image from "next/image";
 import Link from "next/link";
 
@@ -21,13 +23,19 @@ export async function BlockCatalog({ data }: Props) {
         hide={data.blockConfig?.hideBlockTitle ?? false}
         title={data.title}
       />
-      <div className="grid grid-cols-2 gap-4 lg:gap-8 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+      <div className="grid grid-cols-2 gap-4 lg:gap-6 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
         {links?.map((link, idx) => (
           <Link
             href={link?.url ?? ""}
             key={idx}
-            className="relative group flex gap-2 flex-col justify-center rounded-3xl text-lg lg:text-xl"
+            className="relative h-[400px] p-4 group flex flex-col justify-between rounded-3xl text-lg lg:text-xl"
           >
+            <div
+              className={cx(
+                "absolute left-0 right-0 top-0 bottom-0",
+                "transition-all rounded-2xl bg-black opacity-0 group-hover:opacity-10 pointer-events-none",
+              )}
+            />
             {link?.image ? (
               <Image
                 width={250}
@@ -36,6 +44,7 @@ export async function BlockCatalog({ data }: Props) {
                 alt={link.id ?? ""}
               />
             ) : null}
+            <span>{link?.name}</span>
           </Link>
         ))}
       </div>
