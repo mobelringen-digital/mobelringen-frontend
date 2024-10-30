a141ebae9f0cce71af1fb21a6f8c75285caf87dc
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2024-10-30 12:02:19 +0200

    MBEL-744 Missing frontpage meta data fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,30 @@
 import React from "react";
 
-import { Page } from "@/modules/page";
-import { CmsPagesQueryDocument } from "@/queries/page.queries";
-import { CmsPagesQuery, CmsPagesQueryVariables } from "@/types";
-import { baseHygraphClient } from "@/utils/lib/graphql";
+import { Metadata } from "next";
 
-async function getPage() {
-  return await baseHygraphClient("GET").request<
-    CmsPagesQuery,
-    CmsPagesQueryVariables
-  >(CmsPagesQueryDocument, {
-    where: { url: "/" },
-  });
+import { Page } from "@/modules/page";
+import { formatMetaTitle } from "@/utils/helpers";
+
+import { getPage } from "./[...url]/actions";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const data = await getPage(`/`);
+  const page = data.pages[0];
+
+  if (page) {
+    return {
+      title: formatMetaTitle(page?.seo?.metaTitle),
+      description: page.seo?.metaDescription,
+    };
+  }
+
+  return {
+    title: "Møbelringen",
+  };
 }
 
 export default async function Home() {
-  const data = await getPage();
+  const data = await getPage("/");
 
   return <Page data={data} />;
 }
