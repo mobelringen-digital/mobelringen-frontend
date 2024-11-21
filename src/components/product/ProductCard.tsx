9ad3b3702b44449ce1f1ca58dadd985a438aa629
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2024-11-21 14:48:29 +0200

    MBEL-788 Fixed issue with carousel onCLick event

--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -32,6 +32,7 @@ export const ProductCard: React.FC<Props> = ({
   return (
     <div className={cx("relative flex w-full flex-col", className)}>
       <Link
+        legacyBehavior={false}
         onClick={onClick ? () => onClick(product) : undefined}
         className="relative flex items-center justify-center bg-warm-grey px-2 lg:px-6 py-8 lg:py-12 rounded-2xl h-[240px] lg:h-[420px]"
         href={`/${product.canonical_url}`}
