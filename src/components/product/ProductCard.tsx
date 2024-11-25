ae50a7c0ab7af7c070a7a35d8b4f1365698ff476
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2024-11-25 18:34:52 +0200

    (chore): Product stock disabled

--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -8,7 +8,6 @@ import { ProductImage } from "@/components/product/ProductImage";
 import { ProductInformation } from "@/components/product/ProductInformation";
 import { ProductLabels } from "@/components/product/ProductLabels";
 import { ProductPricing } from "@/components/product/ProductPricing";
-import { ProductStock } from "@/components/product/ProductStock";
 import { BaseProductDataForCardFragment } from "@/types";
 import { usePriceRange } from "@/utils/hooks/usePriceRange";
 
@@ -60,7 +59,7 @@ export const ProductCard: React.FC<Props> = ({
         <ProductInformation hasAddToCart={hasAddToCart} product={product} />
         <ProductPricing priceRange={priceRange} />
       </div>
-      <ProductStock product={product} />
+      {/*<ProductStock product={product} />*/}
     </div>
   );
 };
