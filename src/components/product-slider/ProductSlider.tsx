719014776cb54a754676f16ea622b413bcfc7623
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2024-11-26 10:26:28 +0200

    MBEL-788 Typo

--- src/components/product-slider/ProductSlider.tsx
+++ src/components/product-slider/ProductSlider.tsx
@@ -57,7 +57,7 @@ const selectItemGTMEvent = (product: BaseProductDataForCardFragment) => {
     items: [
       {
         item_id: product.sku,
-        addable_to_cart: product.addable_to_cart,
+        addable_to_cart: product.addable_to_cart ?? 0,
         item_name: product.name,
         item_brand: product.productBrand?.name,
         price: product.price_range.maximum_price?.final_price.value,
