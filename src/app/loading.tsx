593dd0044abde7399e64c81be8d8f370e5a27dfa
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2024-12-11 13:59:44 +0200

    (chore): Removed unnecessary loading file

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="w-full fixed top-0 left-0 right-0 z-50 pointer-events-none h-1.5">
-      <div className="h-1.5 w-full bg-powder overflow-hidden">
-        <div className="progress w-full h-full bg-red left-right" />
-      </div>
-    </div>
-  );
-}
