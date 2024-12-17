97f0c6ba36784971262d112947cdfdaa8b0dae4a
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2024-12-17 15:22:21 +0200

    (chore) Removed deprecated lightbox

--- src/modules/product/product-gallery/ProductGallery.tsx
+++ src/modules/product/product-gallery/ProductGallery.tsx
@@ -42,7 +42,7 @@ export const ProductGallery: React.FC<Props> = ({ product }) => {
 
   return (
     <>
-      {isOpen !== null ? (
+      {isOpen ? (
         <ProductLightbox
           isOpen={isOpen}
           onClose={() => setIsOpen(false)}
