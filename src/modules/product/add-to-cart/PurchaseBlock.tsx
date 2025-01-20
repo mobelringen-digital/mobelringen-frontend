1d7a5e8b5450753ddfe628665f45e3f9cae63770
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2025-01-20 13:35:41 +0200

    MBEL-932 Removed stock error block

--- src/modules/product/add-to-cart/PurchaseBlock.tsx
+++ src/modules/product/add-to-cart/PurchaseBlock.tsx
@@ -3,14 +3,12 @@
 import React from "react";
 
 import { FormatNumber } from "@/components/_ui/format-number/FormatNumber";
-import { Info } from "@/components/_ui/icons/figma/Info";
 import { QuantityInput } from "@/components/_ui/quantity-input/QuantityInput";
 import { AddToCart } from "@/components/cart/add-to-cart/AddToCart";
 import { useActiveProductData } from "@/modules/product/active-product-data-provider/useActiveProductData";
 import { DeliveryInfo } from "@/modules/product/add-to-cart/DeliveryInfo";
 import { KlarnaInformation } from "@/modules/product/add-to-cart/KlarnaInformation";
 import {
-  Availability,
   BaseCartFragment,
   BaseProductFragment,
   BaseStoreFragment,
@@ -39,9 +37,6 @@ export const PurchaseBlock: React.FC<Props> = ({
   );
   const { activeProductVariant } = useActiveProductData();
 
-  const canBuyCAC =
-    stock?.getProductStock.cac?.availability !== Availability.OutOfStock;
-
   const isVariantNotSelected =
     isTypename(product, ["ConfigurableProduct"]) &&
     !activeProductVariant.variant;
@@ -69,17 +64,17 @@ export const PurchaseBlock: React.FC<Props> = ({
     <div className="bg-white p-4 lg:p-8 rounded-2xl flex flex-col gap-4">
       <DeliveryInfo stock={stock} product={product} />
 
-      {!canBuyCAC ? (
-        <div className="border border-red border-opacity-50 rounded-xl p-2 mb-2">
-          <div className="flex gap-2">
-            <Info fill="#FF3E3E" />
-            <div className="flex flex-col gap-2 text-xs text-red">
-              Dersom varen ikke er på lager for Klikk og hent, kan du alltid
-              bestille den i våre butikker.
-            </div>
-          </div>
-        </div>
-      ) : null}
+      {/*{!canBuyCAC ? (*/}
+      {/*  <div className="border border-red border-opacity-50 rounded-xl p-2 mb-2">*/}
+      {/*    <div className="flex gap-2">*/}
+      {/*      <Info fill="#FF3E3E" />*/}
+      {/*      <div className="flex flex-col gap-2 text-xs text-red">*/}
+      {/*        Dersom varen ikke er på lager for Klikk og hent, kan du alltid*/}
+      {/*        bestille den i våre butikker.*/}
+      {/*      </div>*/}
+      {/*    </div>*/}
+      {/*  </div>*/}
+      {/*) : null}*/}
 
       <div className="flex gap-4 items-center">
         <QuantityInput
