f2ae45ec40904681547de981a658a512e9448231
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2025-02-05 11:34:25 +0200

    MBEL-961 Fixed typings

--- src/modules/page/index.tsx
+++ src/modules/page/index.tsx
@@ -7,15 +7,15 @@ import { MetaData } from "@/components/meta/MetaData";
 import { CmsPageContent, CmsPageType } from "../../app/[...url]/actions";
 
 interface Props {
-  data: CmsPageType;
+  data: CmsPageType | null;
 }
 
 export const Page: React.FC<Props> = ({ data }) => {
   return (
     <Suspense fallback={<PageTopLoader />}>
-      <MetaData data={data.seo} />
+      <MetaData data={data?.seo} />
 
-      {data.content.map((content, index) => {
+      {data?.content.map((content, index) => {
         return (
           <CmsContentLoader
             key={`${content.__typename}=${index}`}
