6b8de739e616f59225af918791d745d2dbdf03a3
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2025-02-05 22:25:32 +0200

    MBEL-961 Reworked hygraph block to be loaded via

--- src/modules/page/index.tsx
+++ src/modules/page/index.tsx
@@ -3,11 +3,10 @@ import React, { Suspense } from "react";
 import { PageTopLoader } from "@/components/_ui/loader/PageTopLoader";
 import { CmsContentLoader } from "@/components/cms/cms-content-loader";
 import { MetaData } from "@/components/meta/MetaData";
-
-import { CmsPageEntity } from "../../app/[...url]/actions";
+import { CmsPageDetailsFragment, Entity } from "@/types";
 
 interface Props {
-  data: CmsPageEntity;
+  data: CmsPageDetailsFragment & { content: Entity[] };
 }
 
 export const Page: React.FC<Props> = ({ data }) => {
