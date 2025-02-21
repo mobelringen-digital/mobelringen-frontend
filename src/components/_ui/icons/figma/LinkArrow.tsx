ad3b6c9544c6cd72d6fc20cf6c118c2024d06075
Vitalijus Zimaitis <vitalijus.zimaitis@gmail.com>
2025-02-21 08:57:58 +0200

    MBEL-859 Showroom stocks

---
+++ src/components/_ui/icons/figma/LinkArrow.tsx
@@ -0,0 +1,28 @@
+export const LinkArrow = () => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <mask
+        id="mask0_4_22518"
+        maskUnits="userSpaceOnUse"
+        x="0"
+        y="0"
+        width="24"
+        height="24"
+      >
+        <rect width="24" height="24" fill="#D9D9D9" />
+      </mask>
+      <g mask="url(#mask0_4_22518)">
+        <path
+          d="M5.55383 19.5L4.5 18.4461L15.9461 6.99998H8.99997V5.5H18.5V15H17V8.0538L5.55383 19.5Z"
+          fill="#1C1B1F"
+        />
+      </g>
+    </svg>
+  );
+};
